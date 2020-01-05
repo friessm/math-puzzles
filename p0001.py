4cee17d422038bcd385655f536f8db34be3a679b
Marco Friess <marco.friess@gmail.com>
2020-01-05 15:06:46 +0100

    Solution to first puzzle.

---
+++ p0001.py
@@ -0,0 +1,13 @@
+# Project 1: https://projecteuler.net/problem=1
+
+def multiple_of_three_or_five(upper_bound):
+    multiples_list = []
+    for number in range(1, upper_bound):
+        if number % 3 == 0 or number % 5 == 0:
+            multiples_list.append(number)
+    return sum(multiples_list)
+
+
+if __name__ == "__main__":
+    print(multiple_of_three_or_five(1000))
+    
