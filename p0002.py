a22857d086957944bb8eaea8977da10d58965237
Marco Friess <marco.friess@gmail.com>
2020-01-09 17:19:35 +0100

    Solution to second puzzle.

---
+++ p0002.py
@@ -0,0 +1,18 @@
+"""
+Solution to Project Euler problem 2
+Problem 1: https://projecteuler.net/problem=2
+"""
+
+def even_fibunacci(number_1, number_2):
+    fib = 0 
+    res = 0
+    
+    # Starting with n=1 and n-1=0. Fn+1 = Fn + Fn-1.
+    while fib <= 4000000:
+        fib = number_1 + number_2       
+        if fib % 2 == 0: res += fib
+        number_1, number_2 = fib, number_1
+    return res
+
+if __name__ == "__main__":
+    print(even_fibunacci(0, 1))
