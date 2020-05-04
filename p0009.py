b8d3ffeebc73660b14238d4ec29a4a7d21ef6715
Marco Friess <marco.friess@gmail.com>
2020-05-04 08:07:42 +0200

    Solution to problem 9.

---
+++ p0009.py
@@ -0,0 +1,27 @@
+"""
+Solution to Project Euler problem 9
+https://projecteuler.net/problem=9
+"""
+
+from math import prod, sqrt
+
+def generate_primitive_triple(s):
+    """ Euclid's formula.
+
+    https://en.wikipedia.org/wiki/Pythagorean_triple
+
+    """
+    
+    for m in range(2, int(sqrt(s / 2))):   
+        # Increment n and keep m > n > 0.
+        for n in range(1, m) : 
+            # Switch a and b from Euclid's formula to ensure a > b
+            b = m * m - n * n 
+            a = 2 * m * n 
+            c = m * m + n * n 
+
+            if sum([a, b, c]) == s:
+                return prod([a, b, c])
+
+if __name__ == '__main__':
+    print(generate_primitive_triple(1000))
